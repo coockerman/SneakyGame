e43cad30b0073f76922907d9fadee9ba6d8a54ae
hoagg-2603 <hoangnh.b22cn336@stu.ptit.edu.vn>
2025-01-18 10:34:25 +0700

    Game UI

--- Sneaky/Assets/Scripts/UI/UIManager.cs
+++ Sneaky/Assets/Scripts/UI/UIManager.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] GameObject settingUI;
+    [SerializeField] Slider sliderDad;
+    [SerializeField] Slider sliderDog;
+
+    public void OnUISetting()
+    {
+        settingUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void OffnUISetting()
+    {
+        settingUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeMusic()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeFSX()
     {
-        
+
     }
+
+    public void BackToHome()
+    {
+       
+    }
+
+    public void NextMap()
+    {
+
+    }
+
+    public void Replay()
+    {
+
+    }
+
+
+
+    public void UpdateSliderDad(float count)
+    {
+        sliderDad.value = count;
+    }
+    public void UpdateSliderDog(float count)
+    {
+        sliderDog.value = count;
+    }
+
+
 }
+
